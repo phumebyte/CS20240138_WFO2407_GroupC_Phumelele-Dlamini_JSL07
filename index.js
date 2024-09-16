ae900b730c004e4f46706d2ef220939723f4f6e2
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-16 16:44:52 +0200

    Changed certificate inner HTML

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      certificateContent.innerHTML = `
       <h3>${studentName}</h3>
+      <p>${personalMessage}</p>
+      <p>has completed ${courseName}</p>
     `;
     
       //  Display the modal
